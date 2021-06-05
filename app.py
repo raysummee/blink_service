baefb61dbeb57c95ff9b8b81d1078bd58c4c56b2
Angshuman Barpujari <angshuarin@gmail.com>
2021-06-05 21:47:32 +0530

    added jwt and playback is post, only can be accessed though a jwt

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import json
 import socket
+
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+
 from logic.utils.qr import Qr
-from werkzeug.serving import make_server
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import argparse
 from logic.automations.playback_control import PlaybackControl
 from logic.utils.systemInfo import SystemInfo
 
@@ -15,10 +16,14 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["active"] = True
 db = SQLAlchemy(app)
 
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
 
 @app.route('/')
 def home():
-    # print(SystemInfo().minimalInfo())
+    print(create_access_token(identity="username"))
     return render_template('home.html')
 
 
@@ -33,13 +38,17 @@ def activate_view():
     return json.dumps({"active": app.config["active"]})
 
 
-@app.route('/playback/<command>', methods=["GET"])
-def playback(command):
+@app.route('/playback', methods=["POST"])
+@jwt_required()
+def playback():
+    command = request.form.get("command")
+    if command is None:
+        return "No command found"
     try:
         getattr(PlaybackControl(), command)()
     except AttributeError:
-        return "error"
-    return "done"
+        return "{} is not available".format(command)
+    return "{} is performed".format(command)
 
 
 @app.route('/exit', methods=["GET"])
@@ -60,9 +69,9 @@ if __name__ == '__main__':
     if type(ips) == list:
         print("choose the specified ip")
         for i in range(len(ips)):
-            print(str(i+1)+") "+ips[i])
+            print(str(i + 1) + ") " + ips[i])
         indexIP = int(input())
-        ip = ips[indexIP-1]
+        ip = ips[indexIP - 1]
     else:
         ip = ips
     qrData = SystemInfo().qrInfo(ip)
